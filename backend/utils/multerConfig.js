ee83a84abecce9191428af113bb22d255afd61a4
Basant Kumar Mahato <basantkumar72580@gmail.com>
2025-11-09 22:50:07 +0530

    multer.js

--- backend/utils/multerConfig.js
+++ backend/utils/multerConfig.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const express = require('express');
 express.static()
 
 // Ensure uploads directory exists
